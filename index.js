826815508fea1eeb8ce7dc1129a5cbea8bef2913
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-27 09:12:58 +0000

    add file

---
+++ index.js
@@ -0,0 +1,31 @@
+//document.write('Hello World');
+
+//var hello = 'Hello World';
+//alert(hello);
+//console.log(hello);
+
+//var userName = prompt('Enter your name');
+//var greet = 'Hello, '+ userName;
+//document.write('greet');
+
+var a = Number(prompt('enter a'));
+var firstPart = a +'(x*x)';
+document.write(firstPart);
+
+var b = Number(prompt('enter b'));
+var secondPart = '+'+ b +'x';
+document.write(secondPart);
+
+var c = Number(prompt('enter c'));
+var thirdPart = '+'+ c +'=0';
+document.write(thirdPart);
+
+
+
+
+
+
+
+
+
+
