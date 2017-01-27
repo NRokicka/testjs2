7e2c88bbbdeaa4f762ba3e2b2aa03b06c257ec6a
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-27 11:54:59 +0000

    change content

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-//document.write('Hello World');
-
-//var hello = 'Hello World';
-//alert(hello);
-//console.log(hello);
-
-//var userName = prompt('Enter your name');
-//var greet = 'Hello, '+ userName;
-//document.write('greet');
+// a=1 b=26 c=120
 
 var a = Number(prompt('enter a'));
 var firstPart = a +'(x*x)';
@@ -21,7 +13,31 @@ var thirdPart = '+'+ c +'=0';
 document.write(thirdPart);
 
 
+ function equationDescr(a,b,c) {
+var d, x1, x2, result;
+d=b*b-4*a*c;
 
+if (a==0) {
+ result='<br>answer: a can not=0';  
+}
+else {
+if (d>0) {
+x1=(-b+Math.sqrt(d))/(2*a);
+x2=(-b-Math.sqrt(d))/(2*a);
+result='<br>answer: x1='+x1+', x2='+x2;
+}
+else if (d==0) {
+x1= -b/(2*a);
+result="x="+x1;
+}
+else if(d<0) {
+result='<br>no result';
+}
+}
+return result;
+}
+
+document.write(equationDescr(a,b,c));
 
 
 
