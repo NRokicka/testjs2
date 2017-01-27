1c9d354dbb77fd74a545871b7aa954e20b939f9e
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-01-27 13:45:50 +0000

    add file

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 // a=1 b=26 c=120
+//a=0 b=0 c=120
+//a=0 b=12 c=-12
 
-var a = Number(prompt('enter a'));
+var a = Number(prompt('enter "a"'));
 var firstPart = a +'(x*x)';
 document.write(firstPart);
 
-var b = Number(prompt('enter b'));
+
+var b = Number(prompt('enter "b"'));
 var secondPart = '+'+ b +'x';
 document.write(secondPart);
 
-var c = Number(prompt('enter c'));
+var c = Number(prompt('enter "c"'));
 var thirdPart = '+'+ c +'=0';
 document.write(thirdPart);
 
 
- function equationDescr(a,b,c) {
-var d, x1, x2, result;
-d=b*b-4*a*c;
-
-if (a==0) {
- result='<br>answer: a can not=0';  
-}
-else {
-if (d>0) {
-x1=(-b+Math.sqrt(d))/(2*a);
-x2=(-b-Math.sqrt(d))/(2*a);
-result='<br>answer: x1='+x1+', x2='+x2;
-}
-else if (d==0) {
-x1= -b/(2*a);
-result="x="+x1;
-}
-else if(d<0) {
-result='<br>no result';
-}
-}
-return result;
-}
+function equationDescr(a,b,c) {
+    var d = b*b-4*a*c, x, x1, x2, result;
+   
+    
+    if (a==0, b>0){
+        x=-c/b;
+        result ='<br>x='+x;
+    }
+    else if(a==0, b==0){
+        result='<br>no result';
+    }
+    else if (d>0) {
+        x1=(-b+Math.sqrt(d))/(2*a);
+        x2=(-b-Math.sqrt(d))/(2*a);
+        result='<br>x1='+x1+', x2='+x2;
+    }
+    else if (d==0) {
+        x= -b/(2*a);
+        result="<br>x="+x;
+    }
+    else if(d<0) {
+        result='<br>no result';
+    }
+    return result;
+    }
 
 document.write(equationDescr(a,b,c));
 
